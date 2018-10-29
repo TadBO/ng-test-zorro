8cd1a2df81d234ecf704d3aed7d3a5649cd44958
tadbo <986520756@qq.com>
2018-10-29 20:25:14 +0800

    awite styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, AfterContentInit, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit, AfterContentInit {
+  private isShow: boolean = true;
+  constructor() {
+  }
+  ngOnInit() {
+  }
+  ngAfterContentInit() {
+    this.isShow = false;
+  }
 }
+
